ece8b9d95413729ae1ee814de002922b57cfa42d
Diana Aldunate <diana.aldunate@ucb.edu.bo>
2025-04-23 20:07:45 -0400

    instalacion firebase 23/04/2025

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebaseConfig : {
+    apiKey: "AIzaSyBh2dBoyZ6GuWmwHQ5uUXDsmGoeYarl6nU",
+    authDomain: "ukhu-bb4eb.firebaseapp.com",
+    projectId: "ukhu-bb4eb",
+    storageBucket: "ukhu-bb4eb.firebasestorage.app",
+    messagingSenderId: "839749065518",
+    appId: "1:839749065518:web:fd553f721b0debec2878bf"
+  }
 };
 
 /*
