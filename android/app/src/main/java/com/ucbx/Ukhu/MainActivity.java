b2375b50ae721a84a79ea2665ca04e38c42bde2a
Diana Aldunate <diana.aldunate@ucb.edu.bo>
2025-06-17 13:15:44 -0400

    Android Studio Implementado

---
+++ android/app/src/main/java/com/ucbx/Ukhu/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ucbx.Ukhu;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.ucbx.Ukhu',
   appName: 'UKHU',
   webDir: 'www'
 };
 
-export default config;
+export default config;  
